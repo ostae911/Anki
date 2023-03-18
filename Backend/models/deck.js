a4f78d53a365ae83ffbb22bca819d18dbfcc02a7
ostae911 <staedel.oliver@gmail.com>
2023-03-18 12:31:25 +0100

    Fehler

--- Backend/models/deck.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const deckSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, default: '' }
-});
-
-module.exports = mongoose.model('Deck', deckSchema);
